5a3cdd19b331ffc0af586e4b0ae0f89a02bc5bae
tomor <ptacnikt@gmail.com>
2026-03-23 19:40:52 +0100

    Copy kubeadm scripts to the VM automatically on creation.

--- terraform/envs/kubeadm/main.tf
+++ terraform/envs/kubeadm/main.tf
@@ -6,16 +6,35 @@ terraform {
       source  = "larstobi/multipass"
       version = "~> 1.4"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
+}
+
+locals {
+  scripts_dir    = abspath("${path.module}/../../../scripts/kubeadm")
+  cloud_init_dir = abspath("${path.module}/../../modules/multipass-cluster/cloud-init")
+}
+
+resource "local_file" "kubeadm_cloud_init" {
+  filename = "${local.cloud_init_dir}/.rendered/kubeadm.yaml"
+  content = templatefile("${local.cloud_init_dir}/kubeadm.yaml.tftpl", {
+    k8s_prepare_script_b64           = base64encode(file("${local.scripts_dir}/k8s-prepare.sh"))
+    k8s_init_controlplane_script_b64 = base64encode(file("${local.scripts_dir}/k8s-init-controlplane.sh"))
+  })
 }
 
 module "cluster" {
   source = "../../modules/multipass-cluster"
 
+  depends_on = [local_file.kubeadm_cloud_init]
+
   nodes                   = var.nodes
-  default_cloud_init_file = "${path.module}/../../modules/multipass-cluster/cloud-init/base.yaml"
+  default_cloud_init_file = "${local.cloud_init_dir}/base.yaml"
   cloud_init_files = {
-    control-plane = "${path.module}/../../modules/multipass-cluster/cloud-init/kubeadm.yaml"
-    worker        = "${path.module}/../../modules/multipass-cluster/cloud-init/kubeadm.yaml"
+    control-plane = local_file.kubeadm_cloud_init.filename
+    worker        = local_file.kubeadm_cloud_init.filename
   }
 }
